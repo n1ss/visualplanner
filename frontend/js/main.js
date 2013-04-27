162bc58285a517d4c04ca7d9e5bb4d1bb9475e64
upwards <upwards@yandex.ru>
2013-04-27 20:13:20 +0300

    update main js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -55,14 +55,14 @@ require([
 
   Backbone.history.start({pushState: true});
 
-  User.done(function() {
-    App.openPage('/plans', true);
-  }).on({
-    logged: function() {
-      App.openPage('/plans', true);
-    },
-    logout: function() {
-      App.openPage('/', true);
-    }
-  }, this);
+  // User.done(function() {
+  //   App.openPage('/plans', true);
+  // }).on({
+  //   logged: function() {
+  //     App.openPage('/plans', true);
+  //   },
+  //   logout: function() {
+  //     App.openPage('/', true);
+  //   }
+  // }, this);
 });
