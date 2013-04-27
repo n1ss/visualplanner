15389278e9dfbe4a4510d03e8300d4103b5a0581
upwards <upwards@yandex.ru>
2013-04-27 14:21:52 +0300

    update main page

---
+++ frontend/js/Views/Menu/Login.js
@@ -0,0 +1,61 @@
+/**
+ * Login View
+ */
+define([
+  'app',
+  'backbone',
+  'underscore',
+  'tmpl',
+
+  'Classes/Form',
+  'Classes/Network',
+
+  'Views/Base/View'
+
+], function(App, Backbone, _, tmpl, Form) {
+  var Login = App.Views.BaseView.extend({
+
+	el: '.fn-user-side',
+
+    events: {
+      'submit .fn-login-form': 'login'
+    },
+
+    initialize: function() {
+
+    },
+
+    render: function() {
+      this.$el.html(tmpl.render('Menu/Login'));
+
+      return this;
+    },
+
+    login: function(e) {
+      e.preventDefault();
+
+      var form = new Form(this.$('.fn-login-form'));
+
+      if (form.checkValid()) {
+        App.Network.send({
+          url: '/user/login',
+          data: form.data,
+          type: 'post',
+          context: this,
+          success: function(data) {
+            if (data.status) {
+              alert('login success!');
+            } else {
+              var error = $('<p>').html(data.message);
+              this.$('.messages').addClass('error').html(error);
+            }
+          }
+        });
+      }
+    }
+
+  });
+
+  App.Views.Menu = App.Views.Menu || {};
+  App.Views.Menu.Login = Login;
+});
---
+++ frontend/js/Views/Menu/Menu.js
@@ -0,0 +1,34 @@
+/**
+ * Menu View
+ */
+define([
+  'app',
+  'backbone',
+  'underscore',
+
+  'Views/Menu/Login'
+
+], function(App, Backbone, _) {
+  var Menu = App.Views.BaseView.extend({
+
+    events: {
+      //
+    },
+
+    initialize: function() {
+    	var LoginView = new App.Views.Menu.Login();
+
+    	LoginView.render();
+    },
+
+    render: function() {
+      //
+
+      return this;
+    }
+
+  });
+
+  App.Views.Menu = App.Views.Menu || {};
+  App.Views.Menu.Menu = Menu;
+});
