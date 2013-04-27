aff6480f050da8d69a783a7e60c05df85dc7572f
ikachura <C:\Documents and Settings\1\Application Data\The Bat!>
2013-04-28 01:54:14 +0300

    edit update page

--- frontend/js/Views/User/Account.js
+++ frontend/js/Views/User/Account.js
@@ -6,9 +6,11 @@ define([
 
   'Models/User',
 
+  'Classes/Form',
+
   'Views/Base/View'
 
-], function(App, Backbone, _, tmpl, User) {
+], function(App, Backbone, _, tmpl, User, Form) {
   var Account = App.Views.BaseView.extend({
 
     events: {
@@ -20,7 +22,6 @@ define([
 
     render: function() {
       this.$el.html(tmpl.render('User/Account', User.toJSON()));
-      console.log(User.toJSON());
       return this;
     },
 
@@ -30,20 +31,9 @@ define([
       var form = new Form(this.$('.fn-account-form'));
 
       if (form.checkValid()) {
-        App.Network.send({
-          url: '/api/user/account',
-          data: form.data,
-          type: 'post',
-          context: this,
-          success: function(data) {
-            if (data.status) {
-              User.trigger('logged', data.user);
-            } else {
-              var error = $('<p>').html(data.message);
-              this.$('.messages').addClass('error').html(error);
-            }
-          }
-        });
+        User.set(form.data);
+
+        User.save();
       }
     }
 
