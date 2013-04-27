b482dc151351bae9488fe5d80f1f59487bf0468e
Maksym Mikheienko <maksym.mikheienko@kruschecompany.com>
2013-04-27 15:56:42 +0300

    Plan and UserPlan link

---
+++ server/app/Models/UserPlan.js
@@ -0,0 +1,16 @@
+// Favorite schema
+
+var mongoose = require('mongoose')
+    , Schema = mongoose.Schema
+
+var UserPlanSchema = new Schema({
+    user: { type : Schema.ObjectId, ref : 'User' }
+    , plan: { type : Schema.ObjectId, ref : 'Plan' }
+})
+
+/*
+UserPlanSchema
+    .index({ user: 1, createdAt: -1 })
+*/
+
+mongoose.model('UserPlan', UserPlanSchema)
