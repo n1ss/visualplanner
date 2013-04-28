d6d2dfec5966f4e5d8e7e237c49989a23b5cd1b4
upwards <upwards@yandex.ru>
2013-04-28 19:17:43 +0300

    Revert "CONTRAST UPDATE 1"
    This reverts commit a584ae2aace45f18fac36b170fe2a7153f1e106e.

--- frontend/js/Components/Mindmap.js
+++ frontend/js/Components/Mindmap.js
@@ -34,18 +34,18 @@ define([
       var daysCount = days || 31;
 
       for (var i = 0; i < daysCount; i += 1) {
-        var color = '#404546';
+        var color = '#ECF0F1';
         var height = this.paper.height / daysCount;
         var coord = (height * i).toFixed(0);
-        this.paper.path(["M", 0, coord, "H", this.paper.width]).attr({ stroke: '#404546', 'stroke-width': 2 });
+        this.paper.path(["M", 0, coord, "H", this.paper.width]).attr({ stroke: '#E4E9EA', 'stroke-width': 1 });
 
         if(i == 0) coord = 0;
-        if(i % 2 == 0) color = '#7e8b8d';
+        if(i % 2 == 0) color = '#E4E9EA';
         this.paper.rect(0, coord , 70, height).attr({fill: color, stroke: 0});
         this.paper.text(35, coord, i + 1 + "").attr({
           font: '48px Arial',
           fill: "#fff",
-          opacity: 1
+          opacity: 0.5
         });
       }
 
